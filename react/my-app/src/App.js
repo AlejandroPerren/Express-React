c37014da0e11c8a106d4da55c7e50d20d269a372
Alejandro <aletomasperren@gmail.com>
2024-09-12 15:15:52 -0300

    boostrapt arreglado

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"></link>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"></link>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,8 +19,10 @@ function App() {
         >
           Learn React
         </a>
+        <button className='btn btn-primary'>Crear</button>
       </header>
     </div>
+
   );
 }
 
